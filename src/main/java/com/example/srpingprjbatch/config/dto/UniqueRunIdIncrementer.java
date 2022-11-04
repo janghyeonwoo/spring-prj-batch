ce190d3cb1d668fbf0aadf27918809748170d6f7
SMART-TN-038 <blue672@naver.com>
2022-11-04 17:55:39 +0900

    UniqueRunIdIncrementer 추가

--- src/main/java/com/example/srpingprjbatch/config/dto/UniqueRunIdIncrementer.java
+++ src/main/java/com/example/srpingprjbatch/config/dto/UniqueRunIdIncrementer.java
@@ -4,6 +4,9 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 
+/**
+ * 동일 JOB_PARMETER로 실행 시키고 싶은경우 RunIdIncrementer를 사용하면된다.
+ */
 public class UniqueRunIdIncrementer extends RunIdIncrementer {
     private static final String RUN_ID = "run.id";
 
