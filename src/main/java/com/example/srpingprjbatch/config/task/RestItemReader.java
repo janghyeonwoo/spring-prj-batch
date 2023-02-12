5f3091dbbb9bde6feff8ad953c82816f58769bdf
janghyeonwoo <blue672@naver.com>
2023-02-12 22:52:57 +0900

    RestApiJob 추가

--- src/main/java/com/example/srpingprjbatch/config/task/RestItemReader.java
+++ src/main/java/com/example/srpingprjbatch/config/task/RestItemReader.java
@@ -29,6 +29,14 @@ public class RestItemReader implements ItemReader<List<SyncClub>> {
     public List<SyncClub> read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
         if(!first) return null;
         String url = "";
+
+        /**
+         * JSON Array를 받으려고 제네릭을 사용하였는데 결과를 잘받는다고 생각을 했지만 잘받지 못하였다.. List<SyncClub> 타입으로 받을 거라 생각햇지만 그게 아닌 Linked Hash map로 받고 있었던것이다.
+         * 때문에 JdbcItemWriter에서 ':' 값으로 namedParameter인지 판별을 하고 namedParameter인면 pojo에서 값을 꺼내어 sql을 만든다 하지만 Pojo가 아닌 Linked Hash map이였기 때문에 type에러가 계속 난 것이 었다.
+         * 이것을 ㅎ해결 하기  위하여 String  값으로 바꾸고 Response의 result를 제네릭에서 List<SyncClub>으로 타입을 강제하여 하니 성공적으로 Witer가 작동하였다.
+         */
+
+
         String response = httpUtil.callHttpBody(new SyncClub(), String.class, POST, url, MediaType.APPLICATION_JSON, MediaType.ALL);
         Response res = new ObjectMapper().readValue(response,Response.class);
         List<SyncClub> clubList = res.getResult();
